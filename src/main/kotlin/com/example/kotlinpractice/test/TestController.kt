ff094f5f784cedd19d3d18887be7355915d95a0a
Dominic <dhkim@yanadoocorp.com>
2023-02-08 10:13:52 +0900

    commit test

---
+++ src/main/kotlin/com/example/kotlinpractice/test/TestController.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinpractice.test
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping
+    fun test():String {
+        return "success"
+    }
+
+}
